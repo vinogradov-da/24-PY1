cf51ce0ecac92aaaa92d2befd7376b84711fb228
vinogradov-da <vdana406@gmail.com>
2024-10-23 21:25:49 +0300

    Update Task_1.py

--- Лабораторная работа 1/Task_1.py
+++ Лабораторная работа 1/Task_1.py
@@ -1,5 +1,7 @@
 numbers = [2, -93, -2, 8, None , -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 # TODO заменить значение пропущенного элемента средним арифметическим
-a = (2-93-2+8-44-1-85-14+90-22-90-100-8+38-92-45+67+53+25) /20
-numbers1 = [2, -93, -2, 8, a , -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-print("Измененный список:", numbers1)
+summa_chisel = sum(filter(None,numbers))
+kolichestvo_chisel = len(numbers)
+averege = summa_chisel / kolichestvo_chisel
+numbers [4] = averege
+print("Измененный список:", numbers)
