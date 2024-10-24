73390f0f22631ede0210c8d90c7dcfe74f19f2df
vinogradov-da <vdana406@gmail.com>
2024-10-24 19:37:38 +0300

    Update Task_3.py

--- Лабораторная работа 1/Task_3.py
+++ Лабораторная работа 1/Task_3.py
@@ -4,8 +4,8 @@ pages = 100 #кол-во страниц
 lines_in_page = 50 #кол-во строк
 simvols_in_line = 25 #кол-во символов
 size_one_simvol = 4 #размер 1 символа
-kolichestvo_simvolov = pages * lines_in_page * simvols_in_line
-size_all_simvols = size_one_simvol * kolichestvo_simvolov
+quantity_simvolov = pages * lines_in_page * simvols_in_line
+size_all_simvols = size_one_simvol * quantity_simvolov
 size_in_megabytes = size_all_simvols / 1024 / 1024
-Kolichestvo_books = sizeall_in_megabytes / size_in_megabytes
-print("Количество книг, помещающихся на дискету:", int(Kolichestvo_books))
+quantity_books = sizeall_in_megabytes / size_in_megabytes
+print("Количество книг, помещающихся на дискету:", int(quantity_books))
