a2b044819789adc1c8aaf67625ea2f69282f815a
vinogradov-da <vdana406@gmail.com>
2024-10-25 11:03:49 +0300

    Update Task_3.py

--- Лабораторная работа 1/Task_3.py
+++ Лабораторная работа 1/Task_3.py
@@ -1,11 +1,11 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
 sizeall_in_megabytes = 1.44
-pages = 100 #кол-во страниц
-lines_in_page = 50 #кол-во строк
-simvols_in_line = 25 #кол-во символов
-size_one_simvol = 4 #размер 1 символа
-quantity_simvolov = pages * lines_in_page * simvols_in_line
-size_all_simvols = size_one_simvol * quantity_simvolov
+pages = 100  # кол-во страниц
+lines_in_page = 50  # кол-во строк
+simvols_in_line = 25  # кол-во символов
+size_one_simvol = 4  # размер 1 символа
+number_of_simvols = pages * lines_in_page * simvols_in_line
+size_all_simvols = size_one_simvol * number_of_simvols
 size_in_megabytes = size_all_simvols / 1024 / 1024
-quantity_books = sizeall_in_megabytes / size_in_megabytes
-print("Количество книг, помещающихся на дискету:", int(quantity_books))
+number_of_books = sizeall_in_megabytes / size_in_megabytes
+print("Количество книг, помещающихся на дискету:", int(number_of_books))
