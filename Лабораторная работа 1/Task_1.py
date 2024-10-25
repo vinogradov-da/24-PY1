4fc72bc4ac465e61778c88418d3b0d77ee8b188b
vinogradov-da <vdana406@gmail.com>
2024-10-25 10:59:02 +0300

    Update Task_1.py

--- Лабораторная работа 1/Task_1.py
+++ Лабораторная работа 1/Task_1.py
@@ -1,7 +1,7 @@
 numbers = [2, -93, -2, 8, None , -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 # TODO заменить значение пропущенного элемента средним арифметическим
-summa_chisel = sum(filter(None,numbers))
-quantity_chisel = len(numbers)
-averege = summa_chisel / quantity_chisel
-numbers[4] = averege
+sum_numbers = sum(filter(None, numbers))
+number_of_numbers = len(numbers)
+average = sum_numbers / number_of_numbers
+numbers[4] = average
 print("Измененный список:", numbers)
