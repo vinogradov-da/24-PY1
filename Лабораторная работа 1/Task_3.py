1e08e66bc8ba6a61b626aff3283538c52e4f3dbb
vinogradov-da <vdana406@gmail.com>
2024-10-25 14:57:43 +0300

    Update Task_3.py

--- Лабораторная работа 1/Task_3.py
+++ Лабораторная работа 1/Task_3.py
@@ -2,10 +2,10 @@
 sizeall_in_megabytes = 1.44
 pages = 100  # кол-во страниц
 lines_in_page = 50  # кол-во строк
-symbols_in_line = 25  # кол-во символов
-size_one_symbol = 4  # размер 1 символа
-number_of_characters = pages * lines_in_page * symbols_in_line
-size_all_characters = size_one_simbol * number_of_characters
+characters_in_line = 25  # кол-во символов
+size_one_character = 4  # размер 1 символа
+number_of_characters = pages * lines_in_page * characters_in_line
+size_all_characters = size_one_character * number_of_characters
 size_in_megabytes = size_all_characters / 1024 / 1024
 number_of_books = sizeall_in_megabytes / size_in_megabytes
 print("Количество книг, помещающихся на дискету:", int(number_of_books))
