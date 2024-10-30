100ae1261cb35a3875b4cf627c78e79f9937dfbe
vinogradov-da <vdana406@gmail.com>
2024-10-30 09:31:37 +0300

    Update Task2.py

--- Лабораторная работа 2/Task2.py
+++ Лабораторная работа 2/Task2.py
@@ -1,12 +1,9 @@
-from calendar import month
-
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
- # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 money_capital = spend - salary
 for i in range(1, months):
-    spend *= (1 + increase)
+    spend *= 1 + increase
     money_capital += spend - salary
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", int(money_capital) + 1)
