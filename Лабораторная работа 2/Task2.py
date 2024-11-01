0b30ae4f318fe3e1f9055082af86d18e0d72d468
vinogradov-da <vdana406@gmail.com>
2024-11-01 12:01:46 +0300

    Update Task2.py

--- Лабораторная работа 2/Task2.py
+++ Лабораторная работа 2/Task2.py
@@ -1,3 +1,6 @@
+import math
+
+
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
@@ -6,4 +9,4 @@ money_capital = spend - salary
 for i in range(1, months):
     spend *= 1 + increase
     money_capital += spend - salary
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", int(money_capital) + 1)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", math.ceil(money_capital))
