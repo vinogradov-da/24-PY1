22242d1bbca8ee9f3db8e473729575d46e6e1818
vinogradov-da <vdana406@gmail.com>
2024-11-08 16:07:57 +0300

    Update Task_2.py

--- Лабораторная работа 3/Task_2.py
+++ Лабораторная работа 3/Task_2.py
@@ -1,12 +1,12 @@
-def find_common_participants(first, second, sign = ','):
-    first = set(first.split(sign))
-    second = set(second.split(sign))
+def find_common_participants(first_group, second_group, sign = ','):
+    first = set(first_group.split(sign))
+    second = set(second_group.split(sign))
     common_participants = first.intersection(second)
     return sorted(list(common_participants))
 
 
-participants_first_group = "Иванов|Петров|Сидоров"
-participants_second_group = "Петров|Сидоров|Смирнов"
+first_group = "Иванов|Петров|Сидоров"
+second_group = "Петров|Сидоров|Смирнов"
 
-common_participants = find_common_participants(participants_first_group, participants_second_group, sign = '|')
+common_participants = find_common_participants(first_group, second_group, sign = '|')
 print(common_participants)
